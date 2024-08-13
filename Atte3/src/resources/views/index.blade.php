3f887dd5a66a02084fc0bd244720bff0041c5fd0
samemaru1540 <taeka1100@icloud.com>
2024-08-13 22:00:32 +0900

    index変更

---
+++ Atte3/src/resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="attendance__alert">
+  // メッセージ機能
+</div>
+
+<div class="attendance__content">
+  <div class="attendance__panel">
+    <form class="attendance__button">
+      <button class="attendance__button-submit" type="submit">勤務開始</button>
+    </form>
+    <form class="attendance__button">
+      <button class="attendance__button-submit" type="submit">勤務終了</button>
+    </form>
+    <form class="attendance__button">
+      <button class="attendance__button-submit" type="submit">休憩開始</button>
+    </form>
+    <form class="attendance__button">
+      <button class="attendance__button-submit" type="submit">休憩終了</button>
+    </form>
+  </div>
+</div>
+@endsection
