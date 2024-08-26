1ab4887c041b9eab361a31e5cdd250b64cadcaa2
samemaru1540 <taeka1100@icloud.com>
2024-08-26 19:36:15 +0900

    Update CSS files

--- Atte3/src/resources/views/index.blade.php
+++ Atte3/src/resources/views/index.blade.php
@@ -5,23 +5,22 @@
 @endsection
 
 @section('content')
-<div class="attendance__alert">
-  // メッセージ機能
-</div>
-
 <div class="attendance__content">
+  <div class="attendance__alert">
+  // メッセージ機能
+  </div>
   <div class="attendance__panel">
     <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">勤務開始</button>
+      <button class="attendance__button-submit attend" type="submit">勤務開始</button>
     </form>
     <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">勤務終了</button>
+      <button class="attendance__button-submit attend" type="submit">勤務終了</button>
     </form>
     <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">休憩開始</button>
+      <button class="attendance__button-submit break" type="submit">休憩開始</button>
     </form>
     <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">休憩終了</button>
+      <button class="attendance__button-submit break" type="submit">休憩終了</button>
     </form>
   </div>
 </div>
