905a7e78836543727fc338c7615250ffb9a9e5a0
samemaru1540 <taeka1100@icloud.com>
2024-08-28 20:17:38 +0900

    リモートリポジトリの変更

--- Atte3/src/app/Models/User.php
+++ Atte3/src/app/Models/User.php
@@ -41,9 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function times()
-    {
-        return $this->hasMany(Time::class);
-    }
 }
